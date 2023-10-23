02ebc6ac899345d54ab72d2f8b7d4e860973110f
Mac <abdullah.faisal@qbatch.com>
2023-10-23 18:11:22 +0500

    created rest api

---
+++ todo_api/urls.py
@@ -0,0 +1,8 @@
+# todo/todo/urls.py : Main urls.py
+from django.urls import path
+from .views import TodoListApiView, TodoDetailApiView
+
+urlpatterns = [
+    path('api/', TodoListApiView.as_view()),
+    path('api/<int:todo_id>', TodoDetailApiView.as_view())
+]
